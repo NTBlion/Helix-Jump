a092c5af2b354cad50b04e9f727ced5e9426bf1a
ntbLion <ntb.lion@gmail.com>
2022-08-17 16:47:24 +0300

    New Finish #1

--- Assets/Scripts/Platform/FinishPlatformSigment.cs
+++ Assets/Scripts/Platform/FinishPlatformSigment.cs
@@ -2,7 +2,14 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class FinishPlatformSigment : MonoBehaviour
 {
+    public event UnityAction OnFinishGame;
+
+    public void OnTriggerEnter(Collider other)
+    {
+        OnFinishGame?.Invoke();
+    }
 }
