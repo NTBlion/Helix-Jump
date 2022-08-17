baf75d0e70a86c90b7bb94cf93035d17ef355b66
ntbLion <ntb.lion@gmail.com>
2022-08-17 20:38:47 +0300

    Попытка сделать финиш #1

--- Assets/Scripts/Platform/FinishPlatform.cs
+++ Assets/Scripts/Platform/FinishPlatform.cs
@@ -1,7 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class FinishPlatform : Platform
 {
+    public event UnityAction FinishGame;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.TryGetComponent(out Ball ball))
+        {
+            Debug.Log("Я здесь");
+            FinishGame?.Invoke();
+        }
+    }
 }
